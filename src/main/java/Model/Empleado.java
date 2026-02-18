4a2f428210b9a3f968ab046f9f18598e148762ed
Ignacio <ignaciobrenas@gmail.com>
2026-02-18 12:08:18 +0100

    Concesionario (Clase Abstracta Empleado creada)

---
+++ src/main/java/Model/Empleado.java
@@ -0,0 +1,57 @@
+package Model;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import Model.Trabajo;
+
+/**
+ *
+ * @author ignac
+ */
+public abstract class Empleado implements Trabajo {
+protected String nombre;
+protected double sueldoBase = 1200;
+protected int horasTrabajo=8;
+
+    public Empleado(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldoBase() {
+        return sueldoBase;
+    }
+
+    public void setSueldoBase(double sueldoBase) {
+        this.sueldoBase = sueldoBase;
+    }
+
+    public int getHorasTrabajo() {
+        return horasTrabajo;
+    }
+
+    public void setHorasTrabajo(int horasTrabajo) {
+        this.horasTrabajo = horasTrabajo;
+    }
+
+    @Override
+    public String trabajar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double calcularSueldo() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
